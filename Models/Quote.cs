a6cc5cad5f6d425713159e7893bd3a4c9b865ee7
Kevin Campusano <kcampusano@endpointdev.com>
2022-09-25 19:32:49 -0400

    Add required validation to quote name.

--- Models/Quote.cs
+++ Models/Quote.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace QuoteEditorBlazor.Models;
 
 public class Quote
 {
     public int ID { get; set; }
+    [Required]
     public string Name { get; set; }
 }
