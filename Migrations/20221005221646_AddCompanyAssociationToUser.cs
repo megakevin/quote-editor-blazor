40778d638d3dad9fbe9554f6c397661a749dc6e2
Kevin Campusano <kcampusano@endpointdev.com>
2022-10-05 18:53:43 -0400

    Add migration for FK between companies and users.

---
+++ Migrations/20221005221646_AddCompanyAssociationToUser.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuoteEditorBlazor.Migrations
+{
+    public partial class AddCompanyAssociationToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "company_id",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_asp_net_users_company_id",
+                table: "AspNetUsers",
+                column: "company_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_asp_net_users_companies_company_id",
+                table: "AspNetUsers",
+                column: "company_id",
+                principalTable: "companies",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_asp_net_users_companies_company_id",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "ix_asp_net_users_company_id",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "company_id",
+                table: "AspNetUsers");
+        }
+    }
+}
