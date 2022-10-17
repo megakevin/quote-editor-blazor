b596a0ebef2f7bf0e3b317c5b71a3291afd0f594
Kevin Campusano <kcampusano@endpointdev.com>
2022-10-17 19:41:51 -0400

    Update SignalR hub so that users are added to a company specific group when connected and so that quotes changed broadcasts are sent only to other users in the company of the user that made the change.
    Also use authorize attribute to prevent unauthenticated access to the SignalR methods.

--- Hubs/QuotesHub.cs
+++ Hubs/QuotesHub.cs
@@ -1,12 +1,29 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
+using QuoteEditorBlazor.Areas.Identity.Claims;
 
 namespace BlazorServerSignalRApp.Server.Hubs
 {
     public class QuotesHub : Hub
     {
+        [Authorize]
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, GetCompanyQuotesGroupName());
+            await base.OnConnectedAsync();
+        }
+
+        [Authorize]
         public async Task NotifyQuotesChanged()
         {
-            await Clients.Others.SendAsync("QuotesChanged");
+            await Clients.OthersInGroup(GetCompanyQuotesGroupName()).SendAsync("QuotesChanged");
+        }
+
+        private string GetCompanyQuotesGroupName()
+        {
+            var currentCompanyID = Context.User.GetCompanyID();
+            return $"{currentCompanyID}_quotes";
         }
     }
 }
