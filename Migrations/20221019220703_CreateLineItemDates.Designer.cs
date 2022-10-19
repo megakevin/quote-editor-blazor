678c8bfcdddffbfa54860602a630eb34db7ae2ea
Kevin Campusano <kcampusano@endpointdev.com>
2022-10-19 18:10:00 -0400

    Add migration that creates new line item dates table.

--- Migrations/20221019220703_CreateLineItemDates.Designer.cs
+++ Migrations/20221019220703_CreateLineItemDates.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using QuoteEditorBlazor.Data;
@@ -11,9 +12,10 @@ using QuoteEditorBlazor.Data;
 namespace QuoteEditorBlazor.Migrations
 {
     [DbContext(typeof(QuoteEditorContext))]
-    partial class QuoteEditorContextModelSnapshot : ModelSnapshot
+    [Migration("20221019220703_CreateLineItemDates")]
+    partial class CreateLineItemDates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20221019220703_CreateLineItemDates.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace QuoteEditorBlazor.Migrations
+{
+    public partial class CreateLineItemDates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "line_item_dates",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    date = table.Column<DateOnly>(type: "date", nullable: false),
+                    quote_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_line_item_dates", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_line_item_dates_quotes_quote_id",
+                        column: x => x.quote_id,
+                        principalTable: "quotes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_line_item_dates_date",
+                table: "line_item_dates",
+                column: "date");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_line_item_dates_date_quote_id",
+                table: "line_item_dates",
+                columns: new[] { "date", "quote_id" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_line_item_dates_quote_id",
+                table: "line_item_dates",
+                column: "quote_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "line_item_dates");
+        }
+    }
+}
