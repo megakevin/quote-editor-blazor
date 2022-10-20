59ff47cc5fa46324a0f878c948f6016209ab594b
Kevin Campusano <kcampusano@endpointdev.com>
2022-10-20 18:36:20 -0400

    Add migration for new line item table.

---
+++ Migrations/20221020213242_CreateLineItems.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace QuoteEditorBlazor.Migrations
+{
+    public partial class CreateLineItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "line_items",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    unit_price = table.Column<decimal>(type: "numeric(10,2)", precision: 10, scale: 2, nullable: false),
+                    line_item_date_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_line_items", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_line_items_line_item_dates_line_item_date_id",
+                        column: x => x.line_item_date_id,
+                        principalTable: "line_item_dates",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_line_items_line_item_date_id",
+                table: "line_items",
+                column: "line_item_date_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "line_items");
+        }
+    }
+}
